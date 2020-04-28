76c080e1959fd8f8cdecbc46c38cb447ab29f2c2
om rajesh <omrajeshk1994@gmail.com>
2020-04-28 17:58:24 +0530

    Add project files.

---
+++ ShoppingList/Contents/Js/mainScript.js
@@ -0,0 +1,24 @@
+﻿currentList = {}
+function createShoppingList() {
+    currentList.name = $('#txtList').val();
+    currentList.items = new Array();
+
+    if ($('#txtList').val().length > 0) {
+        //web Service Call
+        $('#shoppingListTitle').html(currentList.name);
+        $('ul#shoppingList').empty();
+        $('#NewListDiv').removeClass('d-none');
+        $('#createListDiv').addClass('d-none');
+    }
+} const drawItems = (data) => {
+    var list = $('#shoppingList').empty();
+}
+const addItem = () => {
+    var newItem = {};
+    newItem.name= $('#txtListItem').val();
+    currentList.items.push(newItem);
+    console.log('data', currentList);
+}
+
+$(document).ready(() => {    
+})
